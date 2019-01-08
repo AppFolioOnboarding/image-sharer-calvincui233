0c9870f49583370675c0e808d17e58ee357c1e1b
Calvin Cui <can.cui@appfolio.com>
2019-01-08 10:30:20 -0800

    fix style, delete single line, change to single quote for test description

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -1,7 +1,7 @@
 # articles_controller_test.rb
 require 'test_helper'
 class ApplicationControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get root_path
     assert_response :ok
   end
