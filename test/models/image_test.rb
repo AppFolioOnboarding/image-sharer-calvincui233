64b2f8ba55be80bd18353c900b78b34fd29adee2
Calvin Cui <can.cui@appfolio.com>
2019-01-09 16:49:57 -0800

    CC - make image model and test, new images table

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class ImageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should validate url" do
+    image = Image.new(url: 'http://google.com')
+    assert_predicate(image, :valid?)
+  end
+
+  test "should be invalid url" do
+    image = Image.new(url: 'afnlfaf')
+    refute_predicate image, :valid?
+    assert_includes image.errors.messages[:url], "is not a valid URL"
+  end
+
+  test "should be invalid url, empty case" do
+    image = Image.new
+    refute_predicate image, :valid?
+    assert_includes image.errors.messages[:url], "can't be blank"
+  end
 end
