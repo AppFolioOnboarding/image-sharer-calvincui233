96f45398cad7d0d47da8b8b2192c809bccb4476d
Calvin Cui <can.cui@appfolio.com>
2019-01-14 01:15:24 -0800

    CC - Add navigation bar on home page.

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -5,4 +5,18 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
     get root_path
     assert_response :ok
   end
+
+  test 'should have one link to new' do
+    get root_path
+    assert_select '#new-image-tab' do
+      assert_select 'a[href="/images/new"]', count: 1
+    end
+  end
+
+  test 'should have one link to index' do
+    get root_path
+    assert_select '#all-images-tab' do
+      assert_select 'a[href="/images"]', count: 1
+    end
+  end
 end
