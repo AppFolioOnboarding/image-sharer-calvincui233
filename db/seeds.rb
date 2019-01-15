a8b2581df195e1841dcb85023e3b4ad957392f91
Calvin Cui <can.cui@appfolio.com>
2019-01-15 13:19:26 -0800

    CC - Update image seeds, add tags for some of the image seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 images = [
-  { url: 'https://bit.ly/2siExH7' },
+  { url: 'https://bit.ly/2siExH7', tag_list: 'Santa Barbara, landscape' },
   { url: 'https://bit.ly/2CfKXeF' },
   { url: 'https://bit.ly/2siExH7' },
   { url: 'https://bit.ly/2VMWVpj' },
   { url: 'https://bit.ly/2H8tLxv' },
   { url: 'https://bit.ly/2Ft668B' },
   { url: 'https://bit.ly/2VTF7bM' },
-  { url: 'https://bit.ly/2QGhAaJ' },
+  { url: 'https://bit.ly/2QGhAaJ', tag_list: 'Santa Barbara, building' },
   { url: 'https://bit.ly/2D9GhZL' },
   { url: 'https://bit.ly/2CjwJcJ' },
   { url: 'https://bit.ly/2sqHZ2p' },
-  { url: 'https://bit.ly/2TLgU5J' },
+  { url: 'https://bit.ly/2TLgU5J', tag_list: 'beach, Santa Barbara' },
   { url: 'https://bit.ly/2McJgU4' },
   { url: 'https://bit.ly/2FuqGp2' },
-  { url: 'https://bit.ly/2D8m5qT' },
+  { url: 'https://bit.ly/2D8m5qT', tag_list: 'California, landscape' },
   { url: 'https://bit.ly/2ALVmyL' },
   { url: 'https://bit.ly/2ALvras' },
-  { url: 'https://bit.ly/2SRckD2' },
+  { url: 'https://bit.ly/2SRckD2', tag_list: 'California, Santa Barbara, beach' },
   { url: 'https://bit.ly/2AKtriR' },
   { url: 'https://bit.ly/2FqAUYj' }
 ]
