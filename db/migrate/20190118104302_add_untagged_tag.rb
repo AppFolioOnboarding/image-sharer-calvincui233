fd7c0b0f99a091d22242ac543ec0854d80538c6c
Calvin Cui <can.cui@appfolio.com>
2019-01-18 18:19:18 -0800

    CC - Update migrate file.

--- db/migrate/20190118104302_add_untagged_tag.rb
+++ db/migrate/20190118104302_add_untagged_tag.rb
@@ -1,11 +1,19 @@
 class AddUntaggedTag < ActiveRecord::Migration[5.2]
   def self.up
     execute <<-SQL
+      INSERT INTO tags (name) VALUES ('untagged')
+    SQL
+    execute <<-SQL
+      UPDATE tags SET (taggings_count) = 
+      (SELECT count(DISTINCT images.id) FROM images LEFT JOIN taggings WHERE images.id != taggings.taggable_id)
+      WHERE name='untagged'
+    SQL
+    execute <<-SQL
       INSERT INTO taggings (taggable_id) SELECT DISTINCT images.id FROM images LEFT JOIN taggings 
       WHERE images.id != taggings.taggable_id
     SQL
     execute <<-SQL
-      UPDATE taggings SET (tag_id, taggable_type) = ((SELECT id FROM tags WHERE name='untagged'), 'Image')
+      UPDATE taggings SET (tag_id, taggable_type, context) = ((SELECT id FROM tags WHERE name='untagged'), 'Image', 'tags')
       WHERE tag_id isnull
     SQL
   end
@@ -14,7 +22,7 @@ class AddUntaggedTag < ActiveRecord::Migration[5.2]
     execute <<-SQL
       DELETE FROM taggings WHERE tag_id = (SELECT id FROM tags WHERE name='untagged')
     SQL
-    excuete <<-SQL
+    execute <<-SQL
       DELETE FROM tags WHERE name='untagged'
     SQL
   end
